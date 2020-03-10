dda0f8cd86fa37bf0f5cd88c8dac4891e85098e0
Your Nameksafira <safira.afie@gmail.com>
2020-03-10 19:47:57 +0700

    Add project files.

---
+++ FormLogin.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TP1
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbUsername = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbUsername
+            // 
+            this.lbUsername.AutoSize = true;
+            this.lbUsername.Location = new System.Drawing.Point(35, 62);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(73, 17);
+            this.lbUsername.TabIndex = 0;
+            this.lbUsername.Text = "Username";
+            this.lbUsername.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(35, 109);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(69, 17);
+            this.lbPassword.TabIndex = 1;
+            this.lbPassword.Text = "Password";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.tbUsername.Location = new System.Drawing.Point(124, 59);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(221, 22);
+            this.tbUsername.TabIndex = 2;
+            this.tbUsername.Text = "Username";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.tbPassword.Location = new System.Drawing.Point(124, 109);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(221, 22);
+            this.tbPassword.TabIndex = 3;
+            this.tbPassword.Text = "Password";
+            // 
+            // btLogin
+            // 
+            this.btLogin.Location = new System.Drawing.Point(225, 173);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(120, 35);
+            this.btLogin.TabIndex = 4;
+            this.btLogin.Text = "Login";
+            this.btLogin.UseVisualStyleBackColor = true;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 258);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbUsername);
+            this.Name = "FormLogin";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btLogin;
+    }
+}
+
